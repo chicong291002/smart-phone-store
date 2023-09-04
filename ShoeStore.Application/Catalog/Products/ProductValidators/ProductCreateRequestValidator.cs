cc47179b50d47580a9cc843484b9398eec9e3894
Nguyen Chi Cong <chicongkhpro@gmail.com>
2023-09-04 23:26:40 +0700

    update

---
+++ ShoeStore.Application/Catalog/Products/ProductValidators/ProductCreateRequestValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using ShoeStore.Application.Catalog.Products.DTOS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoeStore.Application.Catalog.Products.ProductValidators
+{
+    public class ProductImageCreateRequestValidator : AbstractValidator<ProductCreateRequest>
+    {
+        public ProductImageCreateRequestValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is Required");
+
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Description is Required")
+                    .MinimumLength(1000).WithMessage("Description is at least 1000 characters");
+
+            RuleFor(x => x.Thumbnail).NotEmpty().WithMessage("Thumbnail is Required");
+
+            RuleFor(x => x.Price).NotEmpty().WithMessage("Price is Required");
+
+            RuleFor(x => x.OriginalPrice).NotEmpty().WithMessage("OriginalPrice is Required");
+
+            RuleFor(x => x.Image).NotEmpty().WithMessage("Image is Required");
+
+        }
+
+    }
+}
---
+++ ShoeStore.Application/Common/PagingRequestBaseValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using ShoeStore.Application.DTOS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoeStore.Application.Common
+{
+    public class PagingRequestBaseValidator : AbstractValidator<PagingRequestBase>
+    {
+        public PagingRequestBaseValidator()
+        {
+            RuleFor(x => x.pageIndex).NotEmpty().WithMessage("pageIndex is Required");
+
+            RuleFor(x => x.pageSize).NotEmpty().WithMessage("pageSize is Required");
+        }
+    }
+}
