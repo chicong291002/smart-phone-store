e4dbdc95bfbba770bd7215406f00ba381bafe46d
Nguyen Chi Cong <chicongkhpro@gmail.com>
2023-09-05 08:18:12 +0700

    Login using MVC Controller

---
+++ ShoeStore.AdminApp/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoeStore.Application.System.Users.DTOS;
+
+namespace ShoeStore.AdminApp.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Login(LoginRequest request)
+        {
+            return View();
+        }
+    }
+}
