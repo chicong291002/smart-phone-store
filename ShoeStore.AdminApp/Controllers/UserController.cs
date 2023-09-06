13c037352a4ef1849bce0abd0bd05a6126e33b65
Nguyen Chi Cong <chicongkhpro@gmail.com>
2023-09-06 12:40:30 +0700

    create add LoginController and Login View share job User (target User) not (target Login)

--- ShoeStore.AdminApp/Controllers/UserController.cs
+++ ShoeStore.AdminApp/Controllers/UserController.cs
@@ -13,7 +13,7 @@ using System.Text;
 namespace ShoeStore.AdminApp.Controllers
 {
     //dang nhap moi dc vao`
-    public class UserController : Controller
+    public class UserController : BaseController
     {
         private readonly IUserApiClient _userApiClient;
         private readonly IConfiguration _configuration;
@@ -40,70 +40,6 @@ namespace ShoeStore.AdminApp.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Login()
-        {
-            //da login roi => chua kip logout ma vao trang login 
-            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Logout()
-        {
-            //da login roi => chua kip logout ma vao trang login 
-            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-            HttpContext.Session.Remove("Token");
-            return RedirectToAction("Login", "User");
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Login(LoginRequest request)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(ModelState);
-            }
-
-            var token = await _userApiClient.Authenticate(request);
-
-            var userPrincipal = this.ValidateToken(token);
-            var authProperties = new AuthenticationProperties
-            {
-                ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
-                IsPersistent = false
-            };
-            HttpContext.Session.SetString("Token", token);
-            //khi Login thanh cong lay token ra 
-
-            await HttpContext.SignInAsync(
-                        CookieAuthenticationDefaults.AuthenticationScheme,
-                        userPrincipal,
-                        authProperties);
-            return RedirectToAction("Index", "Home");// TenFeature :  TenController     
-        }
-
-
-        // giai  ma~ Token ( chua thong tin ve dang nhap ) 
-        private ClaimsPrincipal ValidateToken(string jwtToken)
-        {
-            IdentityModelEventSource.ShowPII = true;
-
-            SecurityToken validatedToken;
-            TokenValidationParameters validationParameters = new TokenValidationParameters();
-
-            validationParameters.ValidateLifetime = true;
-
-            validationParameters.ValidAudience = _configuration["Tokens:Issuer"];
-            validationParameters.ValidIssuer = _configuration["Tokens:Issuer"];
-            validationParameters.IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Tokens:Key"]));
-
-            ClaimsPrincipal principal = new JwtSecurityTokenHandler().ValidateToken(jwtToken, validationParameters, out validatedToken);
-
-            return principal;
-        }
-
-        [HttpGet]
-
         public async Task<IActionResult> Create()
         {
             return View();
