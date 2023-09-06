46c94c1eb71f1d838bbece6a56e7c9a026b68c2f
Nguyen Chi Cong <chicongkhpro@gmail.com>
2023-09-06 11:46:11 +0700

    create base controller manage check Session ( null or not null)

--- ShoeStore.Application/System/Users/DTOS/RegisterRequest.cs
+++ ShoeStore.Application/System/Users/DTOS/RegisterRequest.cs
@@ -1,19 +1,25 @@
-﻿namespace ShoeStore.Application.System.Users.DTOS
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoeStore.Application.System.Users.DTOS
 {
     public class RegisterRequest
     {
-
+        [Display(Name = "Tên")]
         public string firstName { get; set; }
+        [Display(Name = "Họ")]
         public string lastName { get; set; }
+        [Display(Name = "Ngày Sinh")]
         public DateTime Dob { get; set; }
+        [Display(Name = "Hòm Thư")]
         public string email { get; set; }
 
+        [Display(Name = "Số điện thoại")]
         public string phoneNumber { get; set; }
-
+        [Display(Name = "Tài khoản ")]
         public string userName { get; set; }
-
+        [Display(Name = "Mật khẩu")]
         public string passWord { get; set; }
-
+        [Display(Name = "Xác nhận mật khẩu")]
         public string confirmPassword { get; set; }
     }
 }
