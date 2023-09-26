9ebaca40477e4704b7fc166a837ac453334ce913
Nguyen Chi Cong <chicongkhpro@gmail.com>
2023-09-26 23:04:07 +0700

    update

---
+++ ApiIntegration/Orders/IOrderApiClient.cs
@@ -0,0 +1,10 @@
+﻿using ShoeStore.ViewModels.Catalog.Products;
+using ShoeStore.ViewModels.Common;
+
+namespace ShoeStore.AdminApp.ApiIntegration.Products
+{
+    public interface IOrderApiClient
+    {
+       
+    }
+}
---
+++ ApiIntegration/Orders/OrderApiClient.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+using ShoeStore.ViewModels.Catalog.Products;
+using ShoeStore.ViewModels.Common;
+using ShoeStore.ViewModels.Constants;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace ShoeStore.AdminApp.ApiIntegration.Products
+{
+    public class OrderApiClient : IOrderApiClient  
+    {
+        
+    }
+}
